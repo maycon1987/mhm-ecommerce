5df7491b724ab7d5b08bd40f9cdf95d74d032723
maycon1987 <manualdaempresabr@gmail.com>
2026-05-02 22:36:35 -0300

    routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,15 @@
 
+from fastapi import APIRouter
+from app.database import supabase
+
+router = APIRouter()
+
+@router.get("/unidades")
+def listar_unidades():
+    data = supabase.table("unidades").select("*").execute()
+    return data.data
+
+@router.get("/produtos")
+def listar_produtos():
+    data = supabase.table("produtos").select("*").execute()
+    return data.data
