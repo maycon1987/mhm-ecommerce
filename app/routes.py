b44a87b0c9c9886a40f6ce32720ec74c180cc07e
maycon1987 <manualdaempresabr@gmail.com>
2026-05-02 23:41:44 -0300

    routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,16 @@
-from fastapi import Body
+from fastapi import APIRouter, Body
+from app.database import supabase
+
+router = APIRouter()
+
+@router.get("/")
+def home():
+    return {"status": "online"}
+
+@router.get("/unidades")
+def listar_unidades():
+    data = supabase.table("unidades").select("*").execute()
+    return data.data
 
 @router.post("/produtos")
 def criar_produto(produto: dict = Body(...)):
